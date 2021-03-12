0235ea5447fc454bfd46833c10a4ed147f3e7722
Dmitriy Ivanov <atmopunk@outlook.com>
2021-03-12 16:14:55 +0300

    Use postgres as storage

--- src/dialog.py
+++ src/dialog.py
@@ -1,51 +1,39 @@
 import logging
 from collections import namedtuple
-from storage import Storage
 
+from src import storage
 
 Person = namedtuple('Person', ['first_name', 'last_name'])
 session_storage = {}
 
 
-def new_user(user_id, resp):
+def new_user(connection, user_id, resp):
     resp['text'] = 'Привет. Я могу помочь провести стендап, но для начала мне нужно узнать,' \
                                'участников команды. Для этого можно сказать "Добавь в команду ИМЯ".' \
                                'После того, как все люди будут добавлены - можно будет начинать стендап,' \
                                'командой "Начни стендап".'
-    Storage().create_user(user_id)
-    session_storage[user_id] = {'current_speaker': 0, 'held_standup': False}
-    # storage[user_id] = {}
-    # storage[user_id].setdefault('users', [])
-    # storage[user_id]['current_speaker'] = -1
-    # storage[user_id]['held_standup'] = False
+    storage.create_user(connection, user_id)
 
 
-def start_standup(user_id, resp):
+def start_standup(connection, user_id, resp):
     resp['text'] = 'Хорошо, начинаю\n'
-    session_storage[user_id]['current_speaker'] = 0
-    session_storage[user_id]['held_standup'] = True
-    call_next(user_id, resp)
+    storage.start_standup(connection, user_id)
+    call_next(connection, user_id, resp)
 
 
-def call_next(user_id, resp):
-    cur_speaker_idx = session_storage[user_id]['current_speaker']
+def call_next(connection, user_id, resp):
     try:
-        speaker = Storage().get_team_member(user_id, cur_speaker_idx)
+        speaker = storage.call_next_speaker(connection, user_id)
         if 'text' not in resp:
             resp['text'] = f'{speaker["first_name"].capitalize()}, расскажи о прошедшем дне'
         else:
             resp['text'] += f'{speaker["first_name"].capitalize()}, расскажи о прошедшем дне'
-        session_storage[user_id]['current_speaker'] += 1
     except IndexError:
         resp['text'] = 'Это был последний участник команды. Завершаю сессию'
         resp['end_session'] = True
-        del session_storage[user_id]
-        # session_storage[user_id]['current_speaker'] = 0
-        # session_storage[user_id]['held_standup'] = False
 
 
-
-def add_team_member(req, resp):
+def add_team_member(connection, req, resp):
     user_id = req['session']['user']['user_id']
     name_values = req['request']['nlu']['intents']['team.newmember']['slots']['name']['value']
     first_name = name_values.get('first_name', '')
@@ -53,8 +41,7 @@ def add_team_member(req, resp):
     if not first_name:
         resp['text'] = 'К сожалению я не смогла распонзнать имя, попробуйте ещё раз'
         return
-    Storage().add_team_member(user_id, {'first_name': first_name, 'last_name': last_name})
-    # storage[user_id]['users'].append(Person(first_name, last_name))
+    storage.add_team_member(connection, user_id, {'first_name': first_name, 'last_name': last_name})
     logging.info('Added Person(%r,%r) to %r\'s storage', first_name, last_name, user_id)
     resp['text'] = f'Запомнила человека {last_name.capitalize()} {first_name.capitalize()}'
 
@@ -68,33 +55,34 @@ def handle_dialog(req):
         return resp
 
     user_id = req['session']['user']['user_id']
-    if not Storage().check_user_exists(user_id):  # Новый пользователь
-        new_user(user_id, resp)
-        return resp
+    with storage.create_conn() as connection:
+        if not storage.check_user_exists(connection, user_id):  # Новый пользователь
+            new_user(connection, user_id, resp)
+            return resp
 
-    if req['session']['new']:
-        if user_id not in session_storage:
-            session_storage[user_id] = {'current_speaker': 0, 'held_standup': False}
-        resp['text'] = 'Привет. Я тебя помню, твоя команда:'
-        team = Storage().get_team(user_id)
-        for (idx, person) in enumerate(team):
-            resp['text'] += f' {person["last_name"].capitalize()} {person["first_name"].capitalize()}'
-            if idx == len(team):
-                resp['text'] += '.'
+        if req['session']['new']:
+            # if user_id not in session_storage:
+            #     session_storage[user_id] = {'current_speaker': 0, 'held_standup': False}
+            resp['text'] = 'Привет. Я тебя помню, твоя команда:'
+            team = storage.get_team(connection, user_id)
+            for (idx, person) in enumerate(team):
+                resp['text'] += f' {person["last_name"].capitalize()} {person["first_name"].capitalize()}'
+                if idx == len(team):
+                    resp['text'] += '.'
+                else:
+                    resp['text'] += ','
+            return resp
+
+        if storage.check_standup(connection, user_id):
+            if req['request']['command'] == 'у меня все':
+                call_next(user_id, resp)
             else:
-                resp['text'] += ','
+                resp['text'] = ' '  # Игнорируем, если кто-то что-то говорит во время стендапа
+        else:
+            if 'team.newmember' in req['request']['nlu']['intents']:  # Добавление человека в команду
+                add_team_member(connection, req, resp)
+            elif req['request']['command'] == 'начни стендап':
+                start_standup(connection, user_id, resp)
+            else:
+                resp['text'] = 'Неизвестная команда'
         return resp
-
-    if user_id in session_storage and session_storage[user_id]['held_standup']:
-        if req['request']['command'] == 'у меня все':
-            call_next(user_id, resp)
-        else:
-            resp['text'] = ' '  # Игнорируем, если кто-то что-то говорит во время стендапа
-    else:
-        if 'team.newmember' in req['request']['nlu']['intents']:  # Добавление человека в команду
-            add_team_member(req, resp)
-        elif req['request']['command'] == 'начни стендап':
-            start_standup(user_id, resp)
-        else:
-            resp['text'] = 'Неизвестная команда'
-    return resp
--- src/storage.py
+++ src/storage.py
@@ -1,35 +1,86 @@
-import singleton
-import pymongo
+import psycopg2
+import os
 
 
-class Storage(metaclass=singleton.Singleton):
-    def __init__(self):
-        self.client = pymongo.MongoClient('mongodb://localhost:27017')
-        self.db = self.client['alice-standup']
-        self.users = self.db['users']
+def create_conn():
+    return psycopg2.connect(host=os.getenv('PG_HOST'),
+                            user='postgres',
+                            database='postgres',
+                            password=os.getenv('PG_PWD'))
 
-    def create_user(self, user_id):
-        self.users.insert_one({'user_id': user_id, 'members': []})
 
-    def check_user_exists(self, user_id):
-        return self.users.find_one({'user_id': user_id}) is not None
+def start_standup(conn, user_id):
+    cur = conn.cursor()
+    cur.execute("UPDATE users SET standup_held = TRUE WHERE user_id=%s", user_id)
 
-    def add_team_member(self, user_id, person):
-        query = {'user_id': user_id}
-        update = {'$push': {'members': person}}
-        result = self.users.update_one(query, update)
-        if not result.acknowledged or result.modified_count != 1:
-            raise RuntimeError(f'could not update {user_id}')
 
-    def get_team(self, user_id):
-        query = {'user_id': user_id}
-        result = self.users.find_one(query)
-        return result['members']
+def check_standup(conn, user_id):
+    cur = conn.cursor()
+    cur.execute("SELECT standup_held FROM users WHERE user_id = %s", user_id)
+    result = cur.fetchone()
+    return False if result is None else result[0]
 
-    def get_team_member(self, user_id, member_idx):
-        query = {'user_id': user_id}
-        result = self.users.find_one(query)
-        if member_idx > len(result['members']):
-            raise IndexError()
-        return result['members'][member_idx]
+
+def create_user(conn, user_id):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users(user_id, standup_held, cur_speaker) VALUES(%s, False, 0)", user_id)
+    # self.users.insert_one({'user_id': user_id, 'members': []})
+
+
+def check_user_exists(conn, user_id):
+    cur = conn.cursor()
+    cur.execute("SELECT 1 FROM users where user_id = %s", user_id)
+    result = cur.fetchone()
+    return result is not None
+
+    # return self.users.find_one({'user_id': user_id}) is not None
+
+
+def add_team_member(conn, user_id, person):
+    cur = conn.cursor()
+    if 'last_name' in person:
+        cur.execute("INSERT INTO persons(first_name, last_name) VALUES (%s, %s) RETURNING member_id",
+                    person['first_name'],
+                    person['last_name'])
+    else:
+        cur.execute("INSERT INTO persons(first_name, last_name) VALUES (%s) RETURNING member_id", person['first_name'])
+    person_id = cur.fetchone()[0]
+    cur.execute("INSERT_INTO teams(user_id, person_id) VALUES (%s, %s)", user_id, person_id)
+
+
+def get_team(conn, user_id):
+    cur = conn.cursor()
+    cur.execute(
+        'SELECT first_name, last_name FROM persons JOIN teams on teams.person_id = persons.persons_id WHERE user_id=%s',
+        user_id)
+    persons = cur.fetchall()
+    result = []
+    for person in persons:
+        result.append({'first_name': person[0], 'last_name': person[1]})
+    return result
+    #
+    # query = {'user_id': user_id}
+    # result = self.users.find_one(query)
+    # return result['members']
+
+
+def get_team_member(conn, user_id, member_idx):
+    cur = conn.cursor()
+    cur.execute(
+        'SELECT first_name, last_name FROM persons JOIN teams on teams.person_id = persons.persons_id WHERE user_id=%s',
+        user_id)
+    persons = cur.fetchall()
+    # result = []
+    # for person in persons:
+    #     result.append({'first_name': person[0], 'last_name': person[1]})
+    return {'first_name': persons[member_idx][0], 'last_name': persons[member_idx][0]}
+
+
+def call_next_speaker(conn, user_id):
+    cur = conn.cursor()
+    cur.execute("SELECT current_speaker FROM users where user_id = %s", user_id)
+    speaker_num = cur.fetchone()
+    next_speaker = get_team_member(conn, user_id, speaker_num)
+    cur.execute("UPDATE users SET cur_speaker = cur_speaker + 1 WHERE user_id = %s", user_id)
+    return next_speaker
 
